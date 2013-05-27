b8a11326cc0f4491b63fdb6def5bb7fd022430b4
Jocielma Rocha <jocielma.rocha@tre-ma.gov.br>
2013-05-27 11:44:22 -0300

    alteracao 2

--- Shibata.java
+++ Shibata.java
@@ -1,7 +1,7 @@
 class Shibata{
 	public static void main(String args[]){
 		System.out.println("Hello world!");
+		System.out.println("Hello world again!");
 		System.out.println("Hello world again master!");
-
 	}
 }
